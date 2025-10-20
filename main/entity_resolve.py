a389c81d1534abbcc707497ee2e4f898e25b62c9
yinka3 <adedewe.a@northeastern.edu>
2025-10-20 02:06:59 -0400

    added normalized score

--- main/entity_resolve.py
+++ main/entity_resolve.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import faiss
 from sentence_transformers import SentenceTransformer
 from thefuzz import process as fuzzy_process
@@ -7,8 +8,9 @@ class EntityResolver:
     def __init__(self, embedding_model_model='all-MiniLM-L6-v2'):
         self.embedding_model = SentenceTransformer(embedding_model_model)
         self.embedding_dim = 384 
-        self.faiss_index = faiss.IndexFlatL2(self.embedding_dim)
-        self.index_to_id = {}
+        self.faiss_index = faiss.IndexFlatIP(self.embedding_dim)
+        self.f_index_to_id = {}
+        self.id_to_names = {}
         self.fuzzy_choices = {}
         self.entity_profiles = {}
     
@@ -23,14 +25,20 @@ class EntityResolver:
         self.entity_profiles[entity_id] = profile
         
 
-        self.fuzzy_choices[entity_id] = names
+        for name in names:
+            self.fuzzy_choices[name] = entity_id
+        
+        self.id_to_names[entity_id] = names
         
         profile_embedding = self.embedding_model.encode([profile])
-        
+        faiss.normalize_L2(profile_embedding)
         self.faiss_index.add(profile_embedding)
         
         new_index_position = self.faiss_index.ntotal - 1
-        self.index_to_id[new_index_position] = entity_id
+        self.f_index_to_id[new_index_position] = entity_id
+    
+    def update_entity(self, entity_id: str):
+        pass
     
 
     def resolve(self, text: str, context: str, top_k: int = 10, fuzzy_cutoff: int = 80):
@@ -42,24 +50,27 @@ class EntityResolver:
         if choices:
             fuzzy_matches = fuzzy_process.extractBests(query=text, choices=choices.keys(), score_cutoff=fuzzy_cutoff, limit=10)
             for match, score in fuzzy_matches:
-                candidate_ids.add((choices[match], score))
+                candidate_ids.add((choices[match], score, score / 100.00,"fuzzy"))
         
         if self.faiss_index.ntotal > 0:
             query_text = f"{text} mentioned in context of: {context}"
             query_embedding = self.embedding_model.encode([query_text])
+            faiss.normalize_L2(query_embedding)
+            scores, ann = self.faiss_index.search(query_embedding, k=min(top_k, self.faiss_index.ntotal))
 
-            distances, indices = self.faiss_index.search(query_embedding, k=min(top_k, self.faiss_index.ntotal))
-
-            for index_pos, distance in zip(indices[0], distances[0]):
+            for index_pos, score in zip(ann[0], scores[0]):
                 if index_pos != -1:
-                    candidate_ids.add((self.index_to_id[index_pos], distance))
+                    candidate_ids.add((self.f_index_to_id[index_pos], score,(score + 1) / 2),"faiss")
         
         candidates = []
-
-        for entity_id, score in candidate_ids:
+        for entity_id, score, norm, type in candidate_ids:
             candidates.append({
                 "id": entity_id,
-                "score": score,
+                "match_detail": {
+                    "source": type,
+                    "score": score,
+                    "norm_score": norm
+                },
                 "profile": self.entity_profiles[entity_id]
             })
 
